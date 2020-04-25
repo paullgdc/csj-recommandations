5118d379d55d034013924eb4420cc1ec3ffa8eee
Paul le Grand <paul.legranddec@gmail.com>
2020-04-25 13:39:17 -0400

    upvote button

--- frontend/src/Recommandation.svelte
+++ frontend/src/Recommandation.svelte
@@ -1,16 +1,43 @@
 <script>
-    export let name;
-    export let upvoteCount;
+  export let name;
+  export let upvoteCount;
 </script>
 
-<div class="container">
-    <h2>{name}</h2>
-    <h3>upvotes: {upvoteCount}</h3>
-</div>
-
 <style>
-    .container {
-        border: 1px solid black;
-        padding: 1rem 2rem;
-    }
+  .infos {
+    padding: 1rem 0rem;
+  }
+  .container {
+    /* padding: 0rem 2rem; */
+    border: 1px solid black;
+    display: flex;
+    /* justify-content: space-between; */
+    align-items: center;
+  }
+  .upvote {
+    margin: 0rem 1rem;
+    --diameter: 3.2rem;
+    width: var(--diameter);
+    height: var(--diameter);
+    border: 1px solid rgb(0, 0, 0);
+    border-radius: calc(var(--diameter) / 2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: ease-in-out 200ms;
+  }
+
+  .upvote:hover {
+      border-color: rgb(252, 104, 51);
+      color: rgb(252, 104, 51);
+      transform: scale(1.2);
+  }
 </style>
+
+<div class="container">
+  <div class="upvote">+1</div>
+  <div class="infos">
+    <h3>{name}</h3>
+    <h5>upvotes: {upvoteCount}</h5>
+  </div>
+</div>
