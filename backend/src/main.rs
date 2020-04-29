3e087ccc9a0ae3b3dd1fa8f16c3bac0caac13429
Paul le Grand <paullegrand@git.com>
2020-04-28 22:52:30 -0400

    use sql table instead of storing a json

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
+use anyhow::{Result, Context};
 use juniper::FieldResult;
 use num_traits::cast::{FromPrimitive, ToPrimitive};
-use rusqlite::OptionalExtension;
 use tokio;
 use uuid::Uuid;
 use warp::Filter;
@@ -29,6 +28,15 @@ fn config() -> Result<Config> {
     })
 }
 
+fn row_to_rec(row: &rusqlite::Row<'_>) -> rusqlite::Result<Recommandation> {
+    Ok(Recommandation {
+        id: row.get("id")?,
+        name: row.get("name")?,
+        media: row.get("media")?,
+        link: row.get("link")?,
+    })
+}
+
 #[derive(Debug)]
 struct Database {
     conn: rusqlite::Connection,
@@ -47,9 +55,8 @@ impl Database {
             "CREATE TABLE IF NOT EXISTS recommandations(
                 id BLOB PRIMARY KEY NOT NULL,
                 name TEXT NOT NULL,
-                media TEXT NOT NULL,
-                link TEXT,
-                FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+                media INTEGER NOT NULL,
+                link TEXT
             );",
             rusqlite::params![],
         )?;
@@ -72,24 +79,63 @@ impl Database {
     fn all_recommandations(&self) -> Result<Vec<Recommandation>> {
         self.conn
             .prepare("SELECT id, name, media, link FROM recommandations")?
-            .query_map(rusqlite::NO_PARAMS, |row| 
-                Ok(Recommandation {
-                    id: row.get(0)?,
-                    name: row.get(0)?,
-                    media: row.get(0)?,
-                    link: row.get(0)?,
-                })
-            )?
+            .query_map(rusqlite::NO_PARAMS, row_to_rec)?
             .map(|reco| Ok(reco?))
             .collect()
     }
 
-    fn upvotes_by_recommandation_id(&self, reco_id: Uuid) -> Result<Vec<i32>> {
+    fn create_recommandation(&self, new_reco: &Recommandation) -> Result<()> {
         self.conn
-            .prepare("SELECT user_id FROM upvotes WHERE reco_id=?1")?
-            .query_map(rusqlite::params![&reco_id], |row| row.get(0))?
+            .prepare(
+                "INSERT INTO recommandations(id, name, media, link)
+                VALUES(?1, ?2, ?3, ?4)",
+            )?
+            .execute(rusqlite::params![
+                &new_reco.id,
+                &new_reco.name,
+                &new_reco.media,
+                &new_reco.link
+            ])?;
+        Ok(())
+    }
+
+    fn upvotes_by_recommandation_id(&self, reco_id: Uuid) -> Result<Vec<String>> {
+        self.conn
+            .prepare("SELECT user_id FROM upvotes WHERE reco_id=?1 AND vote=1")?
+            .query_map(rusqlite::params![&reco_id], |row| row.get("user_id"))?
             .map(|data| Ok(data?))
             .collect()
+    }
+
+    fn upvote_by_id(&self, reco_id: Uuid, user_id: &str) -> Result<i32> {
+        Ok(self
+            .conn
+            .prepare("SELECT IFNULL((SELECT vote FROM upvotes WHERE user_id=?1 AND reco_id=?2), 0) vote")?
+            .query_row(rusqlite::params![&user_id, &reco_id], |row| row.get("vote"))?)
+    }
+
+    fn recommandation_by_id(&self, reco_id: Uuid) -> Result<Recommandation> {
+        Ok(self
+            .conn
+            .prepare("SELECT id, name, media, link FROM recommandations WHERE id=?1")?
+            .query_row(rusqlite::params![&reco_id], row_to_rec)?)
+    }
+
+    fn flip_upvote(&self, reco_id: Uuid, user_id: &str) -> Result<()> {
+        self.conn
+            .prepare(
+                "INSERT OR REPLACE INTO upvotes(user_id, reco_id, vote)
+                VALUES (
+                    ?1, ?2,
+                    (IFNULL(
+                        (SELECT vote FROM upvotes WHERE user_id=?1 AND reco_id=?2),
+                        0
+                    )  + 1) % 2
+                );
+            ",
+            )?
+            .execute(rusqlite::params![&user_id, &reco_id])?;
+        Ok(())
     }
 
     fn open(path: &std::path::Path) -> rusqlite::Result<Self> {
@@ -148,11 +194,6 @@ struct Recommandation {
     link: Option<String>,
 }
 
-struct User {
-    id: i64,
-    username: String,
-}
-
 #[juniper::graphql_object(Context = Ctx)]
 impl Recommandation {
     fn id(&self) -> juniper::ID {
@@ -163,16 +204,16 @@ impl Recommandation {
         &self.name
     }
 
-    fn upvotes(&self, ctx: &Ctx) -> FieldResult<Vec<i32>> {
+    fn upvotes(&self, ctx: &Ctx) -> FieldResult<Vec<String>> {
         Ok(ctx.0.get()?.upvotes_by_recommandation_id(self.id)?)
     }
 
-    fn upvote_count(&self) -> i32 {
-        unimplemented!()
+    fn upvote_count(&self, ctx: &Ctx) -> FieldResult<i32> {
+        Ok(ctx.0.get()?.upvotes_by_recommandation_id(self.id)?.len() as i32)
     }
 
-    fn is_upvoted_by(&self, user: String) -> bool {
-        unimplemented!()
+    fn is_upvoted_by(&self, ctx: &Ctx, user_id: juniper::ID) -> FieldResult<bool> {
+        Ok(ctx.0.get()?.upvote_by_id(self.id, &user_id)? == 1)
     }
 
     fn media(&self) -> FieldResult<Media> {
@@ -208,24 +249,27 @@ struct Mutation;
 
 #[juniper::graphql_object(Context = Ctx)]
 impl Mutation {
-    fn create_recommandation(ctx: &Ctx, new: NewRecommandation) -> Option<Recommandation> {
-        let db = ctx.0.get().ok()?;
+    fn create_recommandation(ctx: &Ctx, new: NewRecommandation) -> FieldResult<Recommandation> {
+        let db = ctx.0.get()?;
         let new_todo = Recommandation {
             id: db.new_id(),
             name: new.name,
             link: new.link,
             media: new.media.to_u8().unwrap(),
         };
-        // db.put_recommandation(&new_todo).ok()?;
-        Some(new_todo)
+        db.create_recommandation(&new_todo)?;
+        Ok(new_todo)
     }
 
     fn flip_recommandation_vote(
         ctx: &Ctx,
-        user: String,
-        id: juniper::ID,
-    ) -> Option<Recommandation> {
-        todo!()
+        user_id: juniper::ID,
+        reco_id: juniper::ID,
+    ) -> FieldResult<Recommandation> {
+        let db = ctx.0.get()?;
+        let reco_id: Uuid = reco_id.parse()?;
+        db.flip_upvote(reco_id, &user_id)?;
+        Ok(db.recommandation_by_id(reco_id)?)
     }
 }
 
@@ -237,13 +281,13 @@ fn schema() -> Schema {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = config()?;
+    let config = config().context("Wasn't able to read the config")?;
 
     let manager = DatabaseManager {
         path: config.database,
     };
     let pool = DbPool::new(manager)?;
-    pool.get()?.init()?;
+    pool.get()?.init().context("Wasn't able to initialize the database")?;
 
     let ctx = Ctx(pool);
 
